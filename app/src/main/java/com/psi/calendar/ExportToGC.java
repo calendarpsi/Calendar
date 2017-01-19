72d22a70f76584c6d07affe736fbb74193a7b6e7
ProyectoRO <rubentelecouvigo@gmail.com>
2017-01-19 18:00:42 +0100

    2 eventos a Calendario
    Falta:
    
    Modificarlo a una lista de eventos

--- app/src/main/java/com/psi/calendar/ExportToGC.java
+++ app/src/main/java/com/psi/calendar/ExportToGC.java
@@ -1,34 +1,274 @@
 package com.psi.calendar;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import com.google.api.services.calendar.CalendarScopes;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GooglePlayServicesAvailabilityIOException;
+import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.DateTime;
+import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.EventAttendee;
+import com.google.api.services.calendar.model.EventDateTime;
+import com.google.api.services.calendar.model.EventReminder;
+
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class ExportToGC extends AppCompatActivity {
-    private static final List<String> SCOPES = Arrays.asList(CalendarScopes.CALENDAR);
+
+    static final int REQUEST_AUTHORIZATION = 1001;
+    static final int REQUEST_GOOGLE_PLAY_SERVICES = 1002;
+    private static GoogleAccountCredential credential = null;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.export_gc);
-        Button button = (Button) findViewById(R.id.btnEnviarGC);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                try {
-                    addEvent();
-                } catch (IOException e) {
-                    e.printStackTrace();
+        context = getApplicationContext();
+        scheduleEvents();
+
+    }
+
+    public static void setGoogleCredential(GoogleAccountCredential cred){
+        credential = cred;
+    }
+    private void scheduleEvents() {
+
+        GoogleAccountCredential credentials =credential;
+        ArrayList<Event> events = new ArrayList<Event>();
+
+        Event event = new Event()
+                .setSummary("Google I/O 2020")
+                .setLocation("800 Howard St., San Francisco, CA 94103")
+                .setDescription("A chance to hear more about Google's developer products.");
+
+        DateTime startDateTime = new DateTime("2017-01-20T09:00:00+01:00");
+        EventDateTime start = new EventDateTime()
+                .setDateTime(startDateTime)
+                .setTimeZone("Europe/Madrid");
+        event.setStart(start);
+
+        DateTime endDateTime = new DateTime("2017-01-20T11:00:00+01:00");
+        EventDateTime end = new EventDateTime()
+                .setDateTime(endDateTime)
+                .setTimeZone("Europe/Madrid");
+        event.setEnd(end);
+
+        String[] recurrence = new String[] {"RRULE:FREQ=DAILY;COUNT=1"};
+        event.setRecurrence(Arrays.asList(recurrence));
+
+        EventAttendee[] attendees = new EventAttendee[] {
+                new EventAttendee().setEmail("mestrecarting@gmail.com"),
+        };
+        event.setAttendees(Arrays.asList(attendees));
+
+        EventReminder[] reminderOverrides = new EventReminder[] {
+                new EventReminder().setMethod("email").setMinutes(24 * 60),
+                new EventReminder().setMethod("popup").setMinutes(10),
+        };
+        Event.Reminders reminders = new Event.Reminders()
+                .setUseDefault(false)
+                .setOverrides(Arrays.asList(reminderOverrides));
+        event.setReminders(reminders);
+
+        String calendarId = "primary";
+        //event.setId("qwertyuyui");
+        events.add(event);
+
+
+        Event event2 = new Event()
+                .setSummary("Google I/O 2018")
+                .setLocation("800 Howard St., San Francisco, CA 94103")
+                .setDescription("A chance to hear more about Google's developer products.");
+
+        startDateTime = new DateTime("2017-01-20T11:00:00+01:00");
+        start = new EventDateTime()
+                .setDateTime(startDateTime)
+                .setTimeZone("Europe/Madrid");
+        event2.setStart(start);
+
+        endDateTime = new DateTime("2017-01-20T11:30:00+01:00");
+        end = new EventDateTime()
+                .setDateTime(endDateTime)
+                .setTimeZone("Europe/Madrid");
+        event2.setEnd(end);
+
+        recurrence = new String[] {"RRULE:FREQ=DAILY;COUNT=1"};
+        event2.setRecurrence(Arrays.asList(recurrence));
+
+
+        attendees = new EventAttendee[] {
+                new EventAttendee().setEmail("mestrecarting@gmail.com"),
+        };
+        event.setAttendees(Arrays.asList(attendees));
+
+        reminderOverrides = new EventReminder[] {
+                new EventReminder().setMethod("email").setMinutes(24 * 60),
+                new EventReminder().setMethod("popup").setMinutes(10),
+        };
+        reminders = new Event.Reminders()
+                .setUseDefault(false)
+                .setOverrides(Arrays.asList(reminderOverrides));
+        event2.setReminders(reminders);
+        //event2.setId("asddfgh");
+        events.add(event2);
+
+        new ScheduleEventTask(credentials, events, calendarId).execute();
+
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+    /***
+     * This class exports a group of events to Calendar.
+     *
+     */
+    private class ScheduleEventTask extends AsyncTask<Void, Void, ArrayList<Event>> {
+
+        private final String calendarId;
+        private ArrayList<Event> events;
+        private com.google.api.services.calendar.Calendar service = null;
+        private Exception mLastError;
+
+        /***
+         * @param credential
+         * @param events
+         * @param calendarId
+         */
+        public ScheduleEventTask(GoogleAccountCredential credential, ArrayList<Event> events, String calendarId) {
+
+            this.calendarId = calendarId;
+            this.events = events;
+
+            HttpTransport transport = AndroidHttp.newCompatibleTransport();
+            JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+            service = new com.google.api.services.calendar.Calendar.Builder(
+                    transport, jsonFactory, credential)
+                    .setApplicationName("Calendar")
+                    .build();
+
+
+        }
+
+        @Override
+        protected ArrayList<Event> doInBackground(Void... voids) {
+            ArrayList<Event> eventArrayList = new ArrayList<Event>();
+            try {
+                Log.d("AsyncTask", "doInBackground: jsldajf");
+                for (int i = 0; i < events.size(); i++) {
+                    Event event = service.events().insert(calendarId, events.get(i)).execute();
+
+                    Log.d("doInBackground", "Html event:  "+event.getHtmlLink());
+                    eventArrayList.add(event);
                 }
+            } catch (IOException e) {
+                mLastError = e;
+                cancel(true);
             }
-        });
-    }
-    public void addEvent() throws IOException {
+            return eventArrayList;
+        }
 
-    }
+        /**
+         * Fetch a list of the next 10 events from the primary calendar.
+         *
+         * @return List of Strings describing returned events.
+         * @throws IOException
+         */
 
+
+        @Override
+        protected void onPreExecute() {
+
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<Event> events) {
+            super.onPostExecute(events);
+            // TODO: 21/09/16
+
+            Log.d("onPostExecute", "All tasks scheduled. ");
+        }
+
+        @Override
+        protected void onCancelled() {
+            if (mLastError != null) {
+                if (mLastError instanceof GooglePlayServicesAvailabilityIOException) {
+                    showGooglePlayServicesAvailabilityErrorDialog(
+                            ((GooglePlayServicesAvailabilityIOException) mLastError)
+                                    .getConnectionStatusCode());
+                } else if (mLastError instanceof UserRecoverableAuthIOException) {
+                    startActivityForResult(
+                            ((UserRecoverableAuthIOException) mLastError).getIntent(),
+                            ExportToGC.REQUEST_AUTHORIZATION);
+                } else {
+                    Log.d("onCancelled", "onCancelled: "+ mLastError.getMessage());
+                }
+            } else {
+                Log.d("onCancelled", "Request cancelled. ");
+            }
+        }
+    }
+    /**
+     * Display an error dialog showing that Google Play Services is missing
+     * or out of date.
+     * @param connectionStatusCode code describing the presence (or lack of)
+     *     Google Play Services on this device.
+     */
+    void showGooglePlayServicesAvailabilityErrorDialog(
+            final int connectionStatusCode) {
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        Dialog dialog = apiAvailability.getErrorDialog(
+                ExportToGC.this,
+                connectionStatusCode,
+                ExportToGC.REQUEST_GOOGLE_PLAY_SERVICES);
+        dialog.show();
+    }
+    /**
+     * Called when an activity launched here (specifically, AccountPicker
+     * and authorization) exits, giving you the requestCode you started it with,
+     * the resultCode it returned, and any additional data from it.
+     * @param requestCode code indicating which activity result is incoming.
+     * @param resultCode code indicating the result of the incoming
+     *     activity result.
+     * @param data Intent (containing result data) returned by incoming
+     *     activity result.
+     */
+    @Override
+    protected void onActivityResult(
+            int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch(requestCode) {
+            case REQUEST_GOOGLE_PLAY_SERVICES:
+                if (resultCode != RESULT_OK) {
+                    Toast.makeText(ExportToGC.this, "This app requires Google Play Services. Please install " +
+                            "Google Play Services on your device and relaunch this app.", Toast.LENGTH_SHORT).show();
+                } else {
+                    scheduleEvents();
+                }
+                break;
+            case REQUEST_AUTHORIZATION:
+                if (resultCode == RESULT_OK) {
+                    scheduleEvents();
+                }
+                break;
+        }
+    }
 }
--- app/src/main/java/com/psi/calendar/LoginActivity.java
+++ app/src/main/java/com/psi/calendar/LoginActivity.java
@@ -1,122 +1,281 @@
 package com.psi.calendar;
 
+import android.accounts.AccountManager;
+import android.app.Dialog;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.OptionalPendingResult;
-import com.google.android.gms.common.api.ResultCallback;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.util.ExponentialBackOff;
+import com.google.api.services.calendar.CalendarScopes;
 
-public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+import java.util.Arrays;
+import java.util.List;
 
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
+
+public class LoginActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks {
+
+    static final int REQUEST_ACCOUNT_PICKER = 1000;
+    static final int REQUEST_AUTHORIZATION = 1001;
+    static final int REQUEST_GOOGLE_PLAY_SERVICES = 1002;
+    static final int REQUEST_PERMISSION_GET_ACCOUNTS = 1003;
+
+    private static final String[] SCOPES = { CalendarScopes.CALENDAR };
+
+    private GoogleAccountCredential mCredential;
     private SignInButton btnSignIn;
     private GoogleApiClient apiClient;
-    private static final int RC_SIGN_IN = 1001;
     private ProgressDialog progressDialog;
+    private TextView mOutputText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         btnSignIn = (SignInButton)findViewById(R.id.sign_in_button);
-
-        //API GOOGLE:
-        GoogleSignInOptions gso =
-                new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                        .requestEmail()
-                        .build();
-
-        apiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this, this)
-                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-                .build();
-
-        //Eventos botones:
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(apiClient);
-                startActivityForResult(signInIntent, RC_SIGN_IN);
+            public void onClick(View view) {
+                btnSignIn.setEnabled(false);
+                calendarLogin();
+                btnSignIn.setEnabled(true);
             }
         });
-
+        // Initialize credentials and service object.
+        mCredential = GoogleAccountCredential.usingOAuth2(
+                getApplicationContext(), Arrays.asList(SCOPES))
+                .setBackOff(new ExponentialBackOff());
     }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-        Toast.makeText(this, "Error de conexion!", Toast.LENGTH_SHORT).show();
-        Log.e("GoogleSignIn", "OnConnectionFailed: " + connectionResult);
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == RC_SIGN_IN) {
-            GoogleSignInResult result =
-                    Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-
-            handleSignInResult(result);
-        }
-    }
-
-    private void handleSignInResult(GoogleSignInResult result) {
-        if (result.isSuccess()) {
-            //Usuario logueado --> Mostramos sus datos
-            GoogleSignInAccount acct = result.getSignInAccount();
-            Intent intent = new Intent(LoginActivity.this,ImportCalendar.class);
-            //intent.putExtra("nombreUsuario", acct.getDisplayName());
-            startActivity(intent);
-            super.finish();
-        } else {
-            //Usuario no logueado --> Lo mostramos como "Desconectado" tenemos que mandar un msj (non feito aun)
-            btnSignIn.setVisibility(View.VISIBLE);
-        }
-    }
-
 
     @Override
     protected void onStart() {
         super.onStart();
-        OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(apiClient);
-        if (opr.isDone()) {
-            GoogleSignInResult result = opr.get();
-            handleSignInResult(result);
+        String accountName = getSharedPreferences("com.psi.calendar",MODE_PRIVATE)
+                .getString("accountName", null);
+        if (accountName != null) {
+            btnSignIn.setEnabled(false);
+            mCredential.setSelectedAccountName(accountName);
+            calendarLogin();
+            btnSignIn.setEnabled(true);
+        }
+    }
+
+    /**
+     * Attempt to call the API, after verifying that all the preconditions are
+     * satisfied. The preconditions are: Google Play Services installed, an
+     * account was selected and the device currently has online access. If any
+     * of the preconditions are not satisfied, the app will prompt the user as
+     * appropriate.
+     */
+    private void calendarLogin() {
+        if (! isGooglePlayServicesAvailable()) {
+            acquireGooglePlayServices();
+        } else if (mCredential.getSelectedAccountName() == null) {
+            chooseAccount();
         } else {
-            showProgressDialog();
-            opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
-                @Override
-                public void onResult(GoogleSignInResult googleSignInResult) {
-                    hideProgressDialog();
-                    handleSignInResult(googleSignInResult);
+            if (! isDeviceOnline()) {
+                Toast.makeText(LoginActivity.this, "Network not available. Logout to retry login.", Toast.LENGTH_SHORT).show();
+            }
+            ExportToGC.setGoogleCredential(mCredential);
+            Intent intent = new Intent(getApplicationContext(),ImportCalendar.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    /**
+     * Check that Google Play services APK is installed and up to date.
+     * @return true if Google Play Services is available and up to
+     *     date on this device; false otherwise.
+     */
+    private boolean isGooglePlayServicesAvailable() {
+        GoogleApiAvailability apiAvailability =
+                GoogleApiAvailability.getInstance();
+        final int connectionStatusCode =
+                apiAvailability.isGooglePlayServicesAvailable(this);
+        return connectionStatusCode == ConnectionResult.SUCCESS;
+    }
+    /**
+     * Attempt to resolve a missing, out-of-date, invalid or disabled Google
+     * Play Services installation via a user dialog, if possible.
+     */
+    private void acquireGooglePlayServices() {
+        GoogleApiAvailability apiAvailability =
+                GoogleApiAvailability.getInstance();
+        final int connectionStatusCode =
+                apiAvailability.isGooglePlayServicesAvailable(this);
+        if (apiAvailability.isUserResolvableError(connectionStatusCode)) {
+            showGooglePlayServicesAvailabilityErrorDialog(connectionStatusCode);
+        }
+    }
+
+    /**
+     * Display an error dialog showing that Google Play Services is missing
+     * or out of date.
+     * @param connectionStatusCode code describing the presence (or lack of)
+     *     Google Play Services on this device.
+     */
+    void showGooglePlayServicesAvailabilityErrorDialog(
+            final int connectionStatusCode) {
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        Dialog dialog = apiAvailability.getErrorDialog(
+                LoginActivity.this,
+                connectionStatusCode,
+                REQUEST_GOOGLE_PLAY_SERVICES);
+        dialog.show();
+    }
+
+    /**
+     * Attempts to set the account used with the API credentials. If an account
+     * name was previously saved it will use that one; otherwise an account
+     * picker dialog will be shown to the user. Note that the setting the
+     * account to use with the credentials object requires the app to have the
+     * GET_ACCOUNTS permission, which is requested here if it is not already
+     * present. The AfterPermissionGranted annotation indicates that this
+     * function will be rerun automatically whenever the GET_ACCOUNTS permission
+     * is granted.
+     */
+    @AfterPermissionGranted(REQUEST_PERMISSION_GET_ACCOUNTS)
+    private void chooseAccount() {
+        if (EasyPermissions.hasPermissions(
+                this, android.Manifest.permission.GET_ACCOUNTS)) {
+            String accountName = getSharedPreferences("com.psi.calendar",MODE_PRIVATE)
+                    .getString("accountName", null);
+            if (accountName != null) {
+                mCredential.setSelectedAccountName(accountName);
+                calendarLogin();
+            } else {
+                // Start a dialog from which the user can choose an account
+                startActivityForResult(
+                        mCredential.newChooseAccountIntent(),
+                        REQUEST_ACCOUNT_PICKER);
+            }
+        } else {
+            // Request the GET_ACCOUNTS permission via a user dialog
+            EasyPermissions.requestPermissions(
+                    this,
+                    "This app needs to access your Google account (via Contacts).",
+                    REQUEST_PERMISSION_GET_ACCOUNTS,
+                    android.Manifest.permission.GET_ACCOUNTS);
+        }
+    }
+
+    /**
+     * Checks whether the device currently has a network connection.
+     * @return true if the device has a network connection, false otherwise.
+     */
+    private boolean isDeviceOnline() {
+        ConnectivityManager connMgr =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        return (networkInfo != null && networkInfo.isConnected());
+    }
+
+    /**
+     * Callback for when a permission is granted using the EasyPermissions
+     * library.
+     * @param requestCode The request code associated with the requested
+     *         permission
+     * @param list The requested permission list. Never null.
+     */
+    @Override
+    public void onPermissionsGranted(int requestCode, List<String> list) {
+        // Do nothing.
+
+    }
+
+    /**
+     * Callback for when a permission is denied using the EasyPermissions
+     * library.
+     * @param requestCode The request code associated with the requested
+     *         permission
+     * @param list The requested permission list. Never null.
+     */
+    @Override
+    public void onPermissionsDenied(int requestCode, List<String> list) {
+        // Do nothing.
+
+    }
+
+
+    /**
+     * Called when an activity launched here (specifically, AccountPicker
+     * and authorization) exits, giving you the requestCode you started it with,
+     * the resultCode it returned, and any additional data from it.
+     * @param requestCode code indicating which activity result is incoming.
+     * @param resultCode code indicating the result of the incoming
+     *     activity result.
+     * @param data Intent (containing result data) returned by incoming
+     *     activity result.
+     */
+    @Override
+    protected void onActivityResult(
+            int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch(requestCode) {
+            case REQUEST_GOOGLE_PLAY_SERVICES:
+                if (resultCode != RESULT_OK) {
+                    mOutputText.setText(
+                            "This app requires Google Play Services. Please install " +
+                                    "Google Play Services on your device and relaunch this app.");
+                } else {
+                    calendarLogin();
                 }
-            });
+                break;
+            case REQUEST_ACCOUNT_PICKER:
+                if (resultCode == RESULT_OK && data != null &&
+                        data.getExtras() != null) {
+                    String accountName =
+                            data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
+                    if (accountName != null) {
+                        SharedPreferences settings =
+                                getSharedPreferences("com.psi.calendar",MODE_PRIVATE);
+                        SharedPreferences.Editor editor = settings.edit();
+                        editor.putString("accountName", accountName);
+                        editor.apply();
+                        mCredential.setSelectedAccountName(accountName);
+                        calendarLogin();
+                    }
+                }
+                break;
+            case REQUEST_AUTHORIZATION:
+                if (resultCode == RESULT_OK) {
+                    calendarLogin();
+                }
+                break;
         }
     }
 
-    private void showProgressDialog() {
-        if (progressDialog == null) {
-            progressDialog = new ProgressDialog(this);
-            progressDialog.setIndeterminate(true);
-        }
-
-        progressDialog.show();
-    }
-
-    private void hideProgressDialog() {
-        if (progressDialog != null && progressDialog.isShowing()) {
-            progressDialog.hide();
-        }
+    /**
+     * Respond to requests for permissions at runtime for API 23 and above.
+     * @param requestCode The request code passed in
+     *     requestPermissions(android.app.Activity, String, int, String[])
+     * @param permissions The requested permissions. Never null.
+     * @param grantResults The grant results for the corresponding permissions
+     *     which is either PERMISSION_GRANTED or PERMISSION_DENIED. Never null.
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(
+                requestCode, permissions, grantResults, this);
     }
 }
-
